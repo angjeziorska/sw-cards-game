b771fcf63dfc0c66ad4650af36ceaa92743a2ec5
Angelika Jeziorska <43811039+angjez@users.noreply.github.com>
2023-11-12 22:24:01 +0100

    refactor(web): make game players iterable

--- apps/web/app/game-players.tsx
+++ apps/web/app/game-players.tsx
@@ -13,20 +13,17 @@ export function GamePlayers() {
       width="100%"
       justifyContent="space-between"
     >
-      <Box flex={0.5} p={4}>
-        <Typography>Player 1: {score.person}</Typography>
-        <GameCard
-          name={drawnCards.person.name}
-          value={drawnCards.person[winningAttributes.person].toString()}
-        />
-      </Box>
-      <Box flex={0.5} p={4}>
-        <Typography>Player 2: {score.starship}</Typography>
-        <GameCard
-          name={drawnCards.starship.name}
-          value={drawnCards.starship[winningAttributes.starship].toString()}
-        />
-      </Box>
+      {["person", "starship"].map((key, i) => (
+        <Box flex={0.5} p={4} key={key}>
+          <Typography>
+            Player {i + 1}: {score[key]}
+          </Typography>
+          <GameCard
+            name={drawnCards[key].name}
+            value={drawnCards[key][winningAttributes[key]].toString()}
+          />
+        </Box>
+      ))}
     </Box>
   ) : null;
 }
